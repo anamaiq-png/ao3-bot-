3e60af470687b040d5916c91dba1d6d82dbca69d
anamaiq-png <anamaiq@gmail.com>
2026-03-19 13:07:06 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,11 @@
-import java.io.FileReader;
-import java.util.Map;
 import java.util.concurrent.*;
-import com.google.gson.Gson;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Gson gson = new Gson();
-        Map config = gson.fromJson(new FileReader("./config.json"), Map.class);
 
-        String rssUrl = (String) config.get("rss_url");
-        String webhook = (String) config.get("discord_webhook");
-        int interval = ((Double) config.get("interval_minutes")).intValue();
+    public static void main(String[] args) throws Exception {
+
+        String rssUrl = "https://archiveofourown.org/tags/The%20Pitt%20(TV)/feed";
+        String webhook = "https://discord.com/api/webhooks/1484217230509211700/KwZkbPyNkTzBEOfdlmTZbH26dWaXowYZp1chXy1xBZQ5zoyAADr-1rihgs-HT399e_Cs";
 
         RSSReader reader = new RSSReader();
         Storage storage = new Storage("seen.json");
@@ -20,19 +15,36 @@ public class Main {
 
         Runnable task = () -> {
             try {
+                System.out.println("Checking for new fanfics...");
+
                 var entries = reader.fetch(rssUrl);
+
                 for (var entry : entries) {
                     String link = entry.getLink();
+
                     if (!storage.hasSeen(link)) {
                         storage.markSeen(link);
-                        notifier.sendEmbed(entry.getTitle(), entry.getAuthor(), link);
+
+                        notifier.sendEmbed(
+                                entry.getTitle(),
+                                entry.getAuthor(),
+                                link
+                        );
+
+                        System.out.println("New fic: " + entry.getTitle());
                     }
                 }
+
                 storage.save();
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
         };
+
+        scheduler.scheduleAtFixedRate(task, 0, 60, TimeUnit.MINUTES);
+    }
+}
 
         scheduler.scheduleAtFixedRate(task, 0, interval, TimeUnit.MINUTES);
     }
