4772c169f85a527bc1598de00637e8b8450cb51f
anamaiq-png <anamaiq@gmail.com>
2026-03-19 13:01:53 -0300

    Update Storage.java

--- Storage.java
+++ Storage.java
@@ -14,7 +14,7 @@ public class Storage {
 
     private void load() {
         try {
-            Reader reader = new FileReader(file);
+            Reader reader = new FileReader("config.json);
             seen = gson.fromJson(reader, Set.class);
             if (seen == null) seen = new HashSet<>();
         } catch (Exception e) {
